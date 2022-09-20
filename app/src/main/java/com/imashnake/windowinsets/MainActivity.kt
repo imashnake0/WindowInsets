9d59f6e19853469cd3115e7b39eb14b86f9cac8f
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-19 22:53:44 -0400

    Cleaned up `MainActivity`

--- app/src/main/java/com/imashnake/windowinsets/MainActivity.kt
+++ app/src/main/java/com/imashnake/windowinsets/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.imashnake.windowinsets.ui.theme.WindowInsetsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WindowInsetsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Text("There you go")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WindowInsetsTheme {
-        Greeting("Android")
     }
 }
