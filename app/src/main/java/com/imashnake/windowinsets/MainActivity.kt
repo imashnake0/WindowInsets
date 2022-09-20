49ba351972db55ecbb84f67a8389c9bdc33da605
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-19 23:06:56 -0400

    Added `TopAppBar`

--- app/src/main/java/com/imashnake/windowinsets/MainActivity.kt
+++ app/src/main/java/com/imashnake/windowinsets/MainActivity.kt
@@ -3,17 +3,22 @@ package com.imashnake.windowinsets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.imashnake.windowinsets.ui.theme.WindowInsetsTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +32,43 @@ class MainActivity : ComponentActivity() {
             }
 
             WindowInsetsTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text("There you go")
+                Scaffold(
+                    topBar = {
+                        MediumTopAppBar(
+                            title = {
+                                Text(
+                                    "Medium TopAppBar",
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis
+                                )
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { /* doSomething() */ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Menu,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick = { /* doSomething() */ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Favorite,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            }
+                        )
+                    }
+                ) { paddingValues ->
+                    Surface(
+                        modifier = Modifier
+                            .padding(paddingValues)
+                            .fillMaxSize(),
+                        color = Color.White
+                    ) {
+                        Text("There you go", color = Color.Black)
+                    }
                 }
             }
         }
